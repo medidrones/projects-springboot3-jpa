17a52c25fd9c180a537c4f6b327f6fff1c7854a1
Jorge Medina <medidrones@gmail.com>
2023-05-13 17:31:00 -0300

    OrderStatus enum

--- src/main/java/com/medicode/projects/entities/Order.java
+++ src/main/java/com/medicode/projects/entities/Order.java
@@ -1,6 +1,7 @@
 package com.medicode.projects.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.medicode.projects.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -20,6 +21,7 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -28,9 +30,10 @@ public class Order implements Serializable {
     public Order(){
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -48,6 +51,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
