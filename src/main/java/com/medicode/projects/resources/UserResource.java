d5c005c29c25329fe2b768c2d91516a9dd996bbd
Jorge Medina <medidrones@gmail.com>
2023-05-13 14:23:24 -0300

    User entity and resource

---
+++ src/main/java/com/medicode/projects/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.medicode.projects.resources;
+
+import com.medicode.projects.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Jorge", "jorge@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
